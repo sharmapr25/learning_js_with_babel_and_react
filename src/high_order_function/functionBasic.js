6905cb85cbe4390b4942bee301ea8f31888f0088
Preeti Sharma <v-preeti.sharma@btpn.com>
2019-06-09 19:56:44 +0700

    added finding lowest number with tests

--- src/high_order_function/functionBasic.js
+++ src/high_order_function/functionBasic.js
@@ -1,4 +1,11 @@
-export default function getHighestNumber(numbers) {
-  return numbers.reduce((num1, num2) => Math.max(num1, num2), 0);
+function getHighestNumber(numbers) {
+  return numbers.reduce((num1, num2) => Math.max(num1, num2));
 }
+
+function getLowestNumber(numbers) {
+  return numbers.reduce((num1, num2) => Math.min(num1, num2));
+}
+
+
+export { getHighestNumber, getLowestNumber };
 
--- test/high_order_function/functionBasic.test.js
+++ test/high_order_function/functionBasic.test.js
@@ -1,4 +1,4 @@
-import getHighestNumber from '../../src/high_order_function/functionBasic';
+import {getHighestNumber, getLowestNumber} from '../../src/high_order_function/functionBasic';
 
 test('getHighestNumber should return 41 from given array', () => {
   const list = [32, 40, 1, 23, 41, 5];
@@ -10,4 +10,10 @@ test('getHighestNumber should return 40 from given array', () => {
   const list = [32, 40, 1, 23, 5];
   const highestNumber = getHighestNumber(list);
   expect(highestNumber).toBe(40);
+});
+
+test('getlowestNumber should return 1 from given array', () => {
+  const list = [32, 40, 1, 23, 41, 5];
+  const lowestNumber = getLowestNumber(list);
+  expect(lowestNumber).toBe(1);
 });
