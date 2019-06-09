c934484974d68a9781e09dbb3a580b12f451e6ba
Preeti Sharma <v-preeti.sharma@btpn.com>
2019-06-09 19:59:28 +0700

    Removed duplication and introduced high order function

--- src/high_order_function/functionBasic.js
+++ src/high_order_function/functionBasic.js
@@ -1,9 +1,13 @@
+function getNumberBasedOn(condition, numbers) {
+  return numbers.reduce((num1, num2) => condition(num1, num2));
+}
+
 function getHighestNumber(numbers) {
-  return numbers.reduce((num1, num2) => Math.max(num1, num2));
+  return getNumberBasedOn(Math.max, numbers);
 }
 
 function getLowestNumber(numbers) {
-  return numbers.reduce((num1, num2) => Math.min(num1, num2));
+  return getNumberBasedOn(Math.min, numbers);
 }
 
 
